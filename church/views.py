698a375b5b8be906b93ef34cfe9e18459ebb07be
adithjaison <adithjaison@gmail.com>
2023-04-08 14:32:16 +0100

    initial commit

---
+++ church/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def declaration(request):
+    return render(request, 'declaration.html')
