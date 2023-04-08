bdc9b4633a323bf0957a36904f3a7fe112962e6e
adithjaison <adithjaison@gmail.com>
2023-04-08 22:01:59 +0100

    fixed contact us via email

--- church/views.py
+++ church/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.core.mail import send_mail
 from django.conf import settings
-import requests
+from django.core.mail import send_mail
+from django.shortcuts import render
+
 
 def home(request):
     return render(request, 'home.html')
@@ -14,11 +14,15 @@ def about(request):
 def contact(request):
     if request.method == 'POST':
         name = request.POST.get('name')
+        print("name", name)
         email = request.POST.get('email')
+        print("email", email)
         subject = request.POST.get('subject')
+        print("subject", subject)
         message = request.POST.get('message')
+        print("message", message)
 
-        # Send email
+        # sending email
         send_mail(
             subject,
             f"Name: {name}\nEmail: {email}\n\n{message}",
@@ -26,19 +30,16 @@ def contact(request):
             [settings.EMAIL_HOST_USER],
             fail_silently=False,
         )
-
         # Send text message
-        url = "https://api.twilio.com/2010-04-01/Accounts/{account_sid}/Messages.json".format(account_sid=settings.TWILIO_ACCOUNT_SID)
-        data = {
-            "From": settings.TWILIO_PHONE_NUMBER,
-            "To": settings.MY_PHONE_NUMBER,
-            "Body": f"Name: {name}\nEmail: {email}\n\n{message}"
-        }
-        auth = (settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-        response = requests.post(url, data=data, auth=auth)
-
+        # url = "https://api.twilio.com/2010-04-01/Accounts/{account_sid}/Messages.json".format(account_sid=settings.TWILIO_ACCOUNT_SID)
+        # data = {
+        #     "From": settings.TWILIO_PHONE_NUMBER,
+        #     "To": settings.MY_PHONE_NUMBER,
+        #     "Body": f"Name: {name}\nEmail: {email}\n\n{message}"
+        # }
+        # auth = (settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        # response = requests.post(url, data=data, auth=auth)
         return render(request, 'success.html')
-
     return render(request, 'contact.html')
 
 
